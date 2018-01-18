a1911ba46cf69e9f61b36e7c6afcc5d76fa1a482
alfredo fernandez <afernandez8781@hotmail.com>
2018-01-18 09:15:23 -0400

    Sección 12 terminada - backend

--- config/config.js
+++ config/config.js
@@ -1,1 +1,5 @@
 module.exports.SEED = 'este-es-un-seed-dificil';
+
+//google
+module.exports.GOOGLE_CLIENT_ID = '371063624694-8r90co50kft04npf7aatm2a1mrudub89.apps.googleusercontent.com';
+module.exports.GOOGLE_SECRET = 'wgZ5lLbVMLDB5KDAPu4vnAeX';
